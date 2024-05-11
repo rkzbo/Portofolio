949f88654ede465009973a329e41a9e325efb80e
Rizky Wibowo <rizky.wib@gmail.com>
2024-05-11 16:59:08 +0700

    Co-authored-by: Mochamad Noor Syamsu <syamsudotdev@users.noreply.github.com>

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string;
+  name: string; //sebelah kiri keyname  //kanan tipe nya
+  schools: string[]; //indicator array of string
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
+// lifecycle API
+// User kirim request -> server (Node/NextJS) -> request data parameter handler di bawah [line12] -> response untuk kirim data kembali ke user
+export default function handler( // ini yang pertama, handler ini bisa apa aja, untuk keperluan kalau error
+  request: NextApiRequest, // request : data yang dikirim oleh browser atau user
+  response: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  response.status(200).json({ name: 'John Doe', schools: ['SD 1', 'SMP 2', 'SMK 3'] });
+  // {"name":"John Doe"}
 }
+
+// HTTP status code https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+// HTTP Code 1xx, 2xx, 3xx, 4xx, 5xx
+// 1xx informational
+// 2xx success
+// 3xx redirect
+// 4xx 5xx server error//browser sama dengan localhost:3000/api/hello
+// 4xx data error yang tampil dikirim dari browser
+
+// response : yang kita kirim ke browser
+// request : data yang dikirim oleh browser atau data yang dikirim oleh user [contohnya ketika user ketik url di browser]
+
+// pertukaran data front-end & back-end pakai json
+// json adalah javascript object notation - reprentasi data asalnya dari javascript
---
+++ pages/hello/world.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'; //buat ngasih tau nodejs kalau ngambil fungsi useEffet & useState di react
+
+type ContentHelloProperties = {
+  children: string;
+}; // tipe data properties - ini adalah custom type
+
+function ContentHello(properties: ContentHelloProperties) {
+  return <p>Hello Page {properties.children}</p>; //fungsi react component
+} //contentHello ini punya children yang bisa dipanggil
+
+// UI thread          ----> render HelloIndex() ----> HTML dengan "name" dan "schools" masih kosong -----> HTML berubah dengan "name" dan "schools" John Doe
+//                                   |                                                                             ^
+//                                   |                                                                             |
+//                                   V                                                                             |
+// background thread  ---->  useEffect (fetch) ----------------------------------------------------> setName, setSchools
+
+export default function HelloIndex() {
+  const [name, setName] = useState(''); // pendekatan asynchronous
+  const [schools, setSchools] = useState([]); //cara kita deklarasi variable //const = constant
+
+  // panggil API bisa pake useEffect / useLayoutEffect
+  useEffect(() => {
+    //useEffect adalah fungsi yang dikasih react, useEffect isinya fungsi, cara penulisannya useEffect(() => {   })
+    // di javascript / typescript, await & promise
+    // return type dari fetch adalah promise
+    fetch('http://localhost:3000/api/hello') //fetch adalah fungsi,fungsinya untuk manggil API di internet, fungsinya adalah rahasia kecuali buka source code langsung
+      .then((response) => response.json()) //fungsi yang kita panggil dari promisenya fetch
+      // bentuk JSON = {"name":"John Doe", "schools": ["blablalba"]}
+      .then((json) => {
+        setName(json.name);
+        setSchools(json.schools);
+      });
+    // .then((json) => console.log('INI HASIL API json.name = ', json.schools)); //console.log adalah fungsi buat nge-print di web browser, json nama parameter, name nya ngambil data dari keyname
+  }, []);
+
+  //content utama hello/world - content utama ditulis
+  return (
+    <>
+      <p>HELLO {name}</p>
+      <p>SCHOOLS {schools}</p>
+      <ul>
+        <li>
+          <p>Hello Page #1</p>
+        </li>
+        <li>
+          <ContentHello>#2</ContentHello>
+        </li>
+        <li>
+          <ContentHello>#3</ContentHello>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+//export default : keyword di typescript
+//bentuk syntax html tapi html yang diextend disama react untuk bisa render react component
